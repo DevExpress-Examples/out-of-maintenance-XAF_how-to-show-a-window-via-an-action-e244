d83ecf1aa289aae4436d0d3cf2e695e6d452a0ca
Andrey <freazeek@mail.ru>
2020-02-18 17:03:14 +0300

    remove unneeded security

--- CS/HowToShowWindowViaAction.Win/WinApplication.Designer.cs
+++ CS/HowToShowWindowViaAction.Win/WinApplication.Designer.cs
@@ -23,50 +23,40 @@ namespace HowToShowWindowViaAction.Win {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
-			this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
-			this.module3 = new HowToShowWindowViaAction.Module.HowToShowWindowViaActionModule();
-			this.module4 = new HowToShowWindowViaAction.Module.Win.HowToShowWindowViaActionWindowsFormsModule();
-			this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
-			this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
-			this.module7 = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
-
-			this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
-			this.securitySimple1 = new DevExpress.ExpressApp.Security.SecuritySimple();
-			this.authenticationActiveDirectory1 = new DevExpress.ExpressApp.Security.AuthenticationActiveDirectory();
-			this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
-			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
-			// 
-			// securitySimple1
-			// 
-			this.securitySimple1.Authentication = this.authenticationActiveDirectory1;
-			this.securitySimple1.UserType = typeof(DevExpress.Persistent.BaseImpl.BasicUser);
-			// 
-			// authenticationActiveDirectory1
-			// 
-			this.authenticationActiveDirectory1.CreateUserAutomatically = true;
-			this.authenticationActiveDirectory1.UserType = typeof(DevExpress.Persistent.BaseImpl.BasicUser);
-			// 
-			// sqlConnection1
-			// 
-			this.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=HowToShowWindowViaAction;Integrated Security=SSPI;Pooling=false";
-			this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
-			// 
-			// HowToShowWindowViaActionWindowsFormsApplication
-			// 
-			this.ApplicationName = "HowToShowWindowViaAction";
-			this.Connection = this.sqlConnection1;
-			this.Modules.Add(this.module1);
-			this.Modules.Add(this.module2);
-			this.Modules.Add(this.module3);
-			this.Modules.Add(this.module4);
-			this.Modules.Add(this.module5);
-			this.Modules.Add(this.module6);
-			this.Modules.Add(this.module7);
-			this.Modules.Add(this.securityModule1);
-			this.Security = this.securitySimple1;
-			this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.HowToShowWindowViaActionWindowsFormsApplication_DatabaseVersionMismatch);
-			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new HowToShowWindowViaAction.Module.HowToShowWindowViaActionModule();
+            this.module4 = new HowToShowWindowViaAction.Module.Win.HowToShowWindowViaActionWindowsFormsModule();
+            this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.module7 = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // module5
+            // 
+            this.module5.AllowValidationDetailsAccess = true;
+            this.module5.IgnoreWarningAndInformationRules = false;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=HowToShowWindowViaAction;Integrated Security=" +
+    "SSPI;Pooling=false";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // HowToShowWindowViaActionWindowsFormsApplication
+            // 
+            this.ApplicationName = "HowToShowWindowViaAction";
+            this.Connection = this.sqlConnection1;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module6);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.module5);
+            this.Modules.Add(this.module7);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.HowToShowWindowViaActionWindowsFormsApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
 		}
 
@@ -79,9 +69,6 @@ namespace HowToShowWindowViaAction.Win {
 		private DevExpress.ExpressApp.Validation.ValidationModule module5;
 		private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule module6;
 		private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule module7;
-		private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
-		private DevExpress.ExpressApp.Security.SecuritySimple securitySimple1;
-		private DevExpress.ExpressApp.Security.AuthenticationActiveDirectory authenticationActiveDirectory1;
 		private System.Data.SqlClient.SqlConnection sqlConnection1;
 	}
 }
